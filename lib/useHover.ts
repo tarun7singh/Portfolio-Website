1124fbbfbac0820451bd7092ebb302c68b77b87e
tarun7singh <taruninsa1@gmail.com>
2021-08-02 17:15:43 +0530

    general fixes

--- lib/useHover.ts
+++ lib/useHover.ts
@@ -5,11 +5,13 @@ export const useHover = <T extends unknown>(): [
   boolean
 ] => {
   const [value, setValue] = useState<boolean>(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const ref: any = useRef<T | null>(null);
   const handleMouseOver = (): void => setValue(true);
   const handleMouseOut = (): void => setValue(false);
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const node: any = ref.current;
     if (node) {
       node.addEventListener("mouseover", handleMouseOver);
