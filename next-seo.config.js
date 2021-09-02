e32c6740c867929f263ce5c8540091b0b5d95653
Tarun Singh <taruninsa1@gmail.com>
2021-09-02 03:19:29 -0700

    seo and position update

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,10 @@
 export default {
-  title: "Portfolio | Tarun Singh",
-  description: "Full Stack web developer @ Toronto",
-  canonical: "https://tarunsingh.dev",
+  title: "Tarun Singh | Dev Portfolio | Full Stack Developer | Freelancer",
+  description: "Tarun Singh | Full Stack developer at Toronto, ON, Canada and Freelancer | Skills : Javascript, Typescript, NodeJS, ReactJS, MySQL, MongoDB, Golang",
+  keywords: "Tarun Singh, Tarun, Singh, Web developer, Full Stack web developer, Potfolio, Javascript, Typescript, NodeJS, ReactJS, MySQL, MongoDB, Golang, freelancer, toronto, canada, upwork",
+  canonical: "https://www.tarunsingh.dev",
   openGraph: {
+    type: "website",
     url: "https://tarusingh.dev",
     title: "Portfolio | Tarun Singh",
     description: "Full Stack web developer @ Toronto",
