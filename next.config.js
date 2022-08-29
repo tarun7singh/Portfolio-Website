3fd1ca7571973740c767962ed3e67d71960d9c7e
Tarun Singh <taruninsa1@gmail.com>
2022-08-28 21:36:33 -0400

    add cache headers

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
     loader: "akamai",
     path: "",
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|png|css|mp3)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
